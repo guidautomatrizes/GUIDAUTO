07dd7d6d42c98a942d80e2ce1a941acd8933a30b
guidautomatrizes <guidautomatrizes@gmail.com>
2026-08-19 09:18:33 -0300

    Add files via upload

--- api/dados.js
+++ api/dados.js
@@ -1,8 +1,5 @@
 import { Redis } from "@upstash/redis";
 
-// Funciona tanto com a integração "Upstash for Redis" da Vercel Marketplace
-// (que cria KV_REST_API_URL / KV_REST_API_TOKEN) quanto com uma conta Upstash
-// própria (UPSTASH_REDIS_REST_URL / UPSTASH_REDIS_REST_TOKEN).
 const redis = new Redis({
   url: process.env.KV_REST_API_URL || process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.KV_REST_API_TOKEN || process.env.UPSTASH_REDIS_REST_TOKEN,
